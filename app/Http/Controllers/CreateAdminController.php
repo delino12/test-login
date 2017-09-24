bd9bf147d865710710ad0a6d4e9a1f91d8b613a4
Ekpoto Liberty <ekpoto.liberty@gmail.com>
2017-09-24 15:26:08 -0700

    Laravel test login setup for multiple interface

---
+++ app/Http/Controllers/CreateAdminController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Admin;
+
+class CreateAdminController extends Controller
+{
+    //
+    public function createAdmin(Admin $admin)
+    {
+    	$admin_name = "Dragon Glass";
+    	$admin_email = "dragonglass@hng.fun";
+    	$admin_password = "glass";
+    	$admin_level = "omega";
+
+    	$admin = new Admin;
+		$admin->name = $admin_name;
+		$admin->email = $admin_email;
+		$admin->password = $admin_password;
+		$admin->level = $admin_level;
+		$admin->save();
+		
+
+		return "1 Admin created succesfully";
+    }
+}
